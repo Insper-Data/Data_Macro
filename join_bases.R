ec96ce3e36c9252ab1f74dc41d841957d62f665f
augustonetto131 <gutaonetto@gmail.com>
2020-09-17 22:17:11 -0300

    Envio do script que junta as bases

---
+++ join_bases.R
@@ -0,0 +1,20 @@
+# Script para juntar as bases que vamos utilizar
+
+library(tidyverse)
+
+
+debt_prop <- read_csv(url("https://raw.githubusercontent.com/Insper-Data/Data_Macro/master/Dataset/tsuda_tidy.csv"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
