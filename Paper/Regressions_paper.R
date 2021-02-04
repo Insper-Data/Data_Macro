262252999d7ff12f934476999c0db73e265c17f7
Mariaclaradrz <mariaclaradrzs@gmail.com>
2021-02-04 21:44:51 +0000

    Regressão com defasagem

--- Paper/Regressions_paper.R
+++ Paper/Regressions_paper.R
@@ -29,24 +29,42 @@ dataset_total_jan_2021 <- read.csv("https://raw.githubusercontent.com/Insper-Dat
 # Setting panel dataset
 panel_dataset <- pdata.frame(dataset_total_jan_2021, index = c("country", "year"))
 
-# Panel dataset for AM
+# Panel dataset for AM and lag added
 panel_dataset_AM <- panel_dataset %>% 
   filter(develop == "AM",
-         country != "United States")
+         country != "United States") %>% 
+  group_by(country) %>% 
+  mutate(lag_debt_to_GDP = dplyr::lag(debt_to_GDP,1), 
+         lag_ln_GDP_percapita_cur_USD = dplyr::lag(ln_GDP_percapita_cur_USD,1),
+         lag_nominal_rate = dplyr::lag(nominal_rate,1),
+         lag_inflation_end = dplyr::lag(inflation_end,1),
+         lag_current_account_percent_GDP = dplyr::lag(current_account_percent_GDP,1),
+         lag_fx_volatility = dplyr::lag(fx_volatility,1),
+         lag_US_nominal_rate = dplyr::lag(US_nominal_rate,1)) 
 
-# Panel dataset for EM
+
+# Panel dataset for EM and lag added
 panel_dataset_EM <- panel_dataset %>% 
-  filter(develop == "EM")
+  filter(develop == "EM") %>% 
+  group_by(country) %>% 
+  mutate(lag_debt_to_GDP = dplyr::lag(debt_to_GDP,1), 
+         lag_ln_GDP_percapita_cur_USD = dplyr::lag(ln_GDP_percapita_cur_USD,1),
+         lag_nominal_rate = dplyr::lag(nominal_rate,1),
+         lag_inflation_end = dplyr::lag(inflation_end,1),
+         lag_current_account_percent_GDP = dplyr::lag(current_account_percent_GDP,1),
+         lag_fx_volatility = dplyr::lag(fx_volatility,1),
+         lag_US_nominal_rate = dplyr::lag(US_nominal_rate,1)) 
 
 
 #--------------------------------------------------------------------------------------------
       # ADVANCED MARKETS
 #--------------------------------------------------------------------------------------------
 
-# Formula:
-formula_AM <- foreign_ex_officials_participation_percent_GDP ~ debt_to_GDP + ln_GDP_percapita_cur_USD + nominal_rate + inflation_end + current_account_percent_GDP + fx_volatility + vix_EUA + US_nominal_rate
+# Formula for dynamic model:
+formula_AM <- foreign_ex_officials_participation_percent_GDP ~ lag_debt_to_GDP + lag_ln_GDP_percapita_cur_USD + lag_nominal_rate + lag_inflation_end + lag_current_account_percent_GDP + lag_fx_volatility + lag_US_nominal_rate + vix_EUA 
 
-# Panel regressions:
+
+# Dynamic Panel regressions:
 reg1.AM <- plm(formula_AM, data = panel_dataset_AM, model = "within", effect = "individual")
 reg2.AM <- plm(formula_AM, data = panel_dataset_AM, model = "within", effect = "time")
 reg3.AM <- plm(formula_AM, data = panel_dataset_AM, model = "within", effect = "twoways")
@@ -68,17 +86,17 @@ stargazer(reg1.AM,
           add.lines = list(c("Country FE", "Yes", "No", "Yes", "No"), c("Year FE", "No", "Yes", "Yes", "No")),
           se = list(reg1.AMc, reg2.AMc, reg3.AMc, reg4.AMc),
           omit.stat = "f",
-           covariate.labels = c("Debt-to-GDP", "ln(GDP per capita)", "Nominal Interest", "Inflation",
-                                "Current Account Balance", "FX Volatility", "VIX EUA", "US Nominal Rate"),
+          covariate.labels = c("Debt-to-GDP (t-1)", "ln(GDP per capita) (t-1)", "Nominal Interest (t-1)",
+                               "Inflation (t-1)", "Current Account Balance (t-1)",
+                               "FX Volatility (t-1)", "US Nominal Rate (t-1)", "VIX EUA"),
           dep.var.labels = c("Foreign Participation in Sovereign Debt (% GDP)"))
-
 
 #--------------------------------------------------------------------------------------------
       # EMERGING MARKETS
 #--------------------------------------------------------------------------------------------
 
 # Formula:
-formula_EM <- foreign_ex_officials_participation_percent_GDP ~ debt_to_GDP + ln_GDP_percapita_cur_USD + nominal_rate + inflation_end +  current_account_percent_GDP + fx_volatility + vix_EUA + US_nominal_rate 
+formula_EM <- formula_EM_dynamic <- foreign_ex_officials_participation_percent_GDP ~ lag_debt_to_GDP + lag_ln_GDP_percapita_cur_USD + lag_nominal_rate + lag_inflation_end + lag_current_account_percent_GDP + lag_fx_volatility + lag_US_nominal_rate + vix_EUA 
 
 # Panel regressions:
 reg1.EM <- plm(formula_EM, data = panel_dataset_EM, model = "within", effect = "individual")
@@ -102,8 +120,8 @@ stargazer(reg1.EM,
           add.lines = list(c("Country FE", "Yes", "No", "Yes", "No"), c("Year FE", "No", "Yes", "Yes", "No")),
           se = list(reg1.EMc, reg2.EMc, reg3.EMc, reg4.EMc),
           omit.stat = "f",
-          covariate.labels = c("Debt-to-GDP", "ln(GDP per capita)", "Nominal Interest",
-                               "Inflation", "Current Account Balance",
-                               "FX Volatility", "VIX EUA", "US Nominal Rate"),
+          covariate.labels = c("Debt-to-GDP (t-1)", "ln(GDP per capita) (t-1)", "Nominal Interest (t-1)",
+                               "Inflation (t-1)", "Current Account Balance (t-1)",
+                               "FX Volatility (t-1)", "US Nominal Rate (t-1)", "VIX EUA"),
           dep.var.labels = c("Foreign Participation in Sovereign Debt (% GDP)"))
 
